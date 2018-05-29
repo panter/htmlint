b714ae2a2bee5b19cd9ebb6a6ce0656b94641820
Falko Schmidt <scf@panter.ch>
2018-05-29 10:04:36 +0200

    Add more specs.

--- spec/parser_spec.js
+++ spec/parser_spec.js
@@ -8,7 +8,7 @@ describe('Parser', () => {
   describe('with valid HTML', () => {
 
     beforeEach(function (done) {
-      this.instance.parse('<a><b></b></a>').on('end', () => {
+      this.instance.parse('<a><b></b><c><d></d></c></a>').on('end', () => {
         done();
       });
     });
@@ -18,7 +18,7 @@ describe('Parser', () => {
     });
   });
 
-  describe('with invalid HTML', () => {
+  describe('with a missing closing tag', () => {
 
     beforeEach(function (done) {
       this.instance.parse('<a><b></a>').on('end', () => {
@@ -28,6 +28,39 @@ describe('Parser', () => {
 
     it('finds two issues', function () {
       expect(this.instance.issues.length).toBe(2);
+      expect(this.instance.issues[0].type).toEqual('mismatch_close_tag');
+      expect(this.instance.issues[1].type).toEqual('unclosed_tag');
+    });
+  });
+
+  describe('with wrongly placed closing tags', () => {
+
+    beforeEach(function (done) {
+      this.instance.parse('<a><b></a></b>').on('end', () => {
+        done();
+      });
+    });
+
+    it('finds two issues', function () {
+      expect(this.instance.issues.length).toBe(2);
+      expect(this.instance.issues[0].type).toEqual('mismatch_close_tag');
+      expect(this.instance.issues[1].type).toEqual('unclosed_tag');
+    });
+  });
+
+  describe('with inconsistent closing tags', () => {
+
+    beforeEach(function (done) {
+      this.instance.parse('<a><b><c></d></a>').on('end', () => {
+        done();
+      });
+    });
+
+    it('finds two issues', function () {
+      expect(this.instance.issues.length).toBe(3);
+      expect(this.instance.issues[0].type).toEqual('mismatch_close_tag');
+      expect(this.instance.issues[1].type).toEqual('mismatch_close_tag');
+      expect(this.instance.issues[2].type).toEqual('unclosed_tag');
     });
   });
 });
