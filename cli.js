e71415111005653b35b7f6b84db611c18988f572
Falko Schmidt <scf@panter.ch>
2018-05-31 14:55:06 +0200

    Use commander.

--- cli.js
+++ cli.js
@@ -2,6 +2,47 @@
 
 'use strict';
 
-const main = require('./src/main');
+const program = require('commander');
+const fs      = require('fs');
+const Parser  = require('./src/parser');
 
-main();
+program
+.option('-i, --indentation <depth>', 'Number of spaces to use for indentation.', process.cwd())
+.option('-n, --ignore-formatting', 'Ignore formatting inconsistencies.')
+.option('-f, --format', 'Format the given file.')
+.version(require('./package.json').version, '-v, --version');
+
+program
+.on('--help', () => {
+  console.log('');
+  console.log('  Description:');
+  console.log('');
+  console.log('    Valdidates structure and formatting of HTML templates.');
+  console.log('');
+  console.log('    Lint files:');
+  console.log('    $ htmlint path/to/some.html');
+  console.log('');
+  console.log('    Autoformat files:');
+  console.log('    $ htmlint -f path/to/some.html');
+  console.log('');
+  console.log('    Made with ♥ by Panter (www.panter.ch).');
+});
+
+program.parse(process.argv);
+
+let path;
+
+if (program.args.length > 0) {
+  path = program.args.length[0];
+} else {
+  console.log('No file specified. Consult --help for instructions.');
+  process.exit(1);
+}
+
+const parser = new Parser(program);
+
+parser.parse(fs.createReadStream(path)).on('end', () => {
+  parser.issues.forEach((issue) => {
+    console.log(issue.print());
+  });
+});
